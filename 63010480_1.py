036bd8b6e9b0272b07aa0b3780279d73e46e70d5
Ntd Chnn <notdakon.may@gmail.com>
2021-08-31 23:58:55 +0700

    change

--- 63010480_1.py
+++ 63010480_1.py
@@ -1,16 +1,25 @@
-class Stack(){
+class Stack():
 
     def __init__(self):    # ใช้สำหรับสร้าง list ว่าง
-
+        self.s = []
 
     def push(self,i):      # เก็บข้อมูลลง stack
+        self.s.append(str(i))
 
     def pop(self):         # นำข้อมูลออกจาก stack
+        self.s.pop()
 
     def isEmpty(self):     # ตรวจสอบว่า stack ว่างไหม ถ้าว่าง return true ถ้าไม่ว่าง return false
+        if len(self.s) != 0:
+            return False
+        else: return True
 
     def size(self):        # ตรวจสอบจำนวนข้อมูลใจ stack
-}
+        return len(self.s)
+
+    def items(self):
+        return self.s
+
 
 print(" *** Stack implement by Python list***")
 
@@ -19,13 +28,11 @@ ls = [e for e in input("Enter data to stack : ").split()]
 s = Stack()
 
 for e in ls:
-
     s.push(e)
 
-print(s.size(),"Data in stack : ",s.items)
+print(s.size(), "Data in stack : ", s.items())
 
 while not s.isEmpty():
-
     s.pop()
 
-print(s.size(),"Data in stack : ",s.items)
+print(s.size(),"Data in stack : ", s.items())
---
+++ 63010480_2.py
@@ -0,0 +1,74 @@
+class ManageStack():
+
+    def __init__(self):
+        self.s = []
+
+    def add(self, i):
+        self.s.append(int(i))
+        print(f'Add = {i}')
+
+    def pop(self):
+        if len(self.s) == 0:
+            print("-1")
+        else:
+            print(f'Pop = {self.s[-1]}')
+            self.s.pop()
+
+    def delete(self, i):
+        self.deL = []
+        if len(self.s) == 0:
+            print("-1")
+        else:
+            while int(i) in self.s:
+                self.s.remove(int(i))
+                print(f'Delete = {i}')
+
+    def lessThen(self, i):
+        self.deL = []
+        if len(self.s) == 0:
+            print("-1")
+        else:
+            for j in range(0, len(self.s)):
+                if self.s[j] < int(i):
+                    self.deL.append(self.s[j])
+            self.deL.reverse()
+            for k in range(0, len(self.deL)):
+                self.s.remove(self.deL[k])
+                print(f'Delete = {self.deL[k]} Because {self.deL[k]} is less than {i}')
+
+    def moreThen(self, i):
+        self.deL = []
+        if len(self.s) == 0:
+            print("-1")
+        else:
+            for j in range(0, len(self.s)):
+                if self.s[j] > int(i):
+                    self.deL.append(self.s[j])
+                    print(f'Delete = {self.s[j]} Because {self.s[j]} is more than {i}')
+            for k in range(0, len(self.deL)):
+                self.s.remove(self.deL[k])
+
+
+    def __str__(self):
+        return f'Value in Stack = {self.s}'
+
+
+
+ip = [e for e in input("Enter Input : ").split(",")]
+
+s = ManageStack()
+
+for i in range(0,len(ip)):
+    if ip[i] == 'P':
+        s.pop()
+    else:
+        command, value = ip[i].split()
+        if command == 'A':
+            s.add(value)
+        elif command == 'D':
+            s.delete(value)
+        elif command == 'LD':
+            s.lessThen(value)
+        else: s.moreThen(value)
+
+print(s)
